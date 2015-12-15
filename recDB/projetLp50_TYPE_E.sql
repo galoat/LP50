529b023e622ad9fbcceb3cd0174aedb92f467797
nicolas <nicolas.lorrain@utbm.fr>
2015-12-15 09:51:30 +0100

    ajout BD

---
+++ recDB/projetLp50_TYPE_E.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE  IF NOT EXISTS `projetLp50` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `projetLp50`;
+-- MySQL dump 10.13  Distrib 5.7.9, for linux-glibc2.5 (x86_64)
+--
+-- Host: localhost    Database: projetLp50
+-- ------------------------------------------------------
+-- Server version	5.6.27-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `TYPE_E`
+--
+
+DROP TABLE IF EXISTS `TYPE_E`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TYPE_E` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TYPE` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `TYPE_E`
+--
+
+LOCK TABLES `TYPE_E` WRITE;
+/*!40000 ALTER TABLE `TYPE_E` DISABLE KEYS */;
+INSERT INTO `TYPE_E` VALUES (1,'séminaire'),(2,'conférence'),(3,'atelier technique'),(4,'atelier déco'),(5,'exposition'),(6,'salon'),(7,'soirée'),(8,'évènement VIP'),(9,'porte ouverte');
+/*!40000 ALTER TABLE `TYPE_E` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-15  9:44:13
